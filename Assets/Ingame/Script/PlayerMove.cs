c6613393183124b6d4dd06e993e7c8e264c2c0a3
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2025-02-16 21:59:16 +0900

    [Add]enum追加

--- Assets/Ingame/Script/PlayerMove.cs
+++ Assets/Ingame/Script/PlayerMove.cs
@@ -7,10 +7,19 @@ public class PlayerMove : MonoBehaviour
 
     Rigidbody _rb;
 
-    private void Start()
+    PlayerState _state;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Init()
     {
         _rb = GetComponent<Rigidbody>();
+        _state = PlayerState.Normal;
     }
+    
     private void FixedUpdate()
     {
         Moving();
@@ -33,18 +42,28 @@ public class PlayerMove : MonoBehaviour
     {
         if (WallCheck() && Input.GetKey(KeyCode.S))
         {
-            _rb.isKinematic = false;
+            _rb.isKinematic = true;
+            _state = PlayerState.Pole;
            
         }
         else if (WallCheck() && Input.GetKey(KeyCode.W))
         {
-            _rb.isKinematic = true;
+            _rb.isKinematic = false;
+            _state = PlayerState.Normal;
         }
     }
 
     private bool WallCheck()
     {
         bool wallForward = Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 0.5f);
+        Debug.DrawRay(transform.position, transform.forward, Color.white);
         return wallForward;
     }
+
+    public enum PlayerState
+    {
+        Normal,Pole,Air    
+    }
+
 }
+
