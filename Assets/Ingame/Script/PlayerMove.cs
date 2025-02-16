141726ac880631acfb6caf8b913af29b634b2540
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2025-02-16 21:34:39 +0900

    [Add]重力の追加 constains設定の追加

--- Assets/Ingame/Script/PlayerMove.cs
+++ Assets/Ingame/Script/PlayerMove.cs
@@ -5,7 +5,12 @@ public class PlayerMove : MonoBehaviour
     [SerializeField]
     float _speed = 5f;
 
+    Rigidbody _rb;
 
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
     private void FixedUpdate()
     {
         Moving();
@@ -15,5 +20,10 @@ public class PlayerMove : MonoBehaviour
         var input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
 
         transform.position += input * _speed;
+    }
+
+    private void AddGravity()
+    {
+        _rb.AddForce(Vector3.down, ForceMode.Acceleration);
     }
 }
