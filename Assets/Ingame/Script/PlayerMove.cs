9b4e62cec9d0660e105038c321f16429113fd8dd
DESKTOP-TIA91N5\lenga <lengatakamuu6200@gmail.com>
2025-02-19 20:19:22 +0900

    [Add]ポールに沿って動く機能の実装

--- Assets/Ingame/Script/PlayerMove.cs
+++ Assets/Ingame/Script/PlayerMove.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -8,7 +9,24 @@ public class PlayerMove : MonoBehaviour
     Rigidbody _rb;
 
     PlayerState _state;
+    
+    CapsuleCollider _capsuleCollider;
 
+    [FormerlySerializedAs("_radiusOffset")] [SerializeField]
+    private float radiusOffset = 0.1f;
+
+    [FormerlySerializedAs("_layerMask")] [SerializeField] 
+    private LayerMask layerMask;
+    
+    [SerializeField]
+    float poleRunSpeed = 5f;
+
+    private float _angle = 0;
+
+    public bool IsGround;
+
+    [FormerlySerializedAs("_length")] [SerializeField] 
+    private float length;
     private void Awake()
     {
         Init();
@@ -16,12 +34,14 @@ public class PlayerMove : MonoBehaviour
 
     private void Init()
     {
+        _capsuleCollider = GetComponent<CapsuleCollider>();
         _rb = GetComponent<Rigidbody>();
         _state = PlayerState.Normal;
     }
     
     private void FixedUpdate()
     {
+        GroundCheck();
         StateForUpdate();
         InOutWallRun();
     }
@@ -49,9 +69,35 @@ public class PlayerMove : MonoBehaviour
 
     private void PoleMoving()
     {
-
+        var collider = GetWall();
+        var pole = collider as CapsuleCollider;
+        Vector3 localUp = pole.transform.up;
+        Vector3 yMovement = localUp * Input.GetAxis("Vertical") * _speed;
+         _angle = Input.GetAxisRaw("Horizontal") * poleRunSpeed ;
+         float radian = -_angle * Mathf.Deg2Rad;
+         var newPosition = transform.position +yMovement;
+         Debug.Log(newPosition);
+         transform.position = newPosition;
+        transform.RotateAround(pole.transform.position, Vector3.up, radian);
+        
     }
 
+    private Collider GetWall()
+    {
+        var bottum = transform.position - Vector3.up * (_capsuleCollider.height / 2 + _capsuleCollider.radius) + _capsuleCollider.center;
+        var top = transform.position + Vector3.up * (_capsuleCollider.height / 2 - _capsuleCollider.radius) + _capsuleCollider.center;
+        var hits = Physics.OverlapCapsule(bottum, top, _capsuleCollider.radius + radiusOffset, layerMask);
+        try
+        {
+            Debug.Log(hits[0].gameObject.name);
+            return hits[0];
+        }
+        catch
+        {
+            return null;    
+        }
+
+    }
     private void AddGravity()
     {
         _rb.AddForce(Vector3.down, ForceMode.Acceleration);
@@ -61,12 +107,12 @@ public class PlayerMove : MonoBehaviour
     {
         if (WallCheck())
         {
-            if (Input.GetKey(KeyCode.W))
+            if (Input.GetKey(KeyCode.W) && IsGround)
             {
                 _rb.isKinematic = true;
                 _state = PlayerState.Pole;
             }
-            else if (Input.GetKey(KeyCode.S))
+            else if (Input.GetKey(KeyCode.S)&& IsGround)
             {
                 _rb.isKinematic = false;
                 _state = PlayerState.Normal;
@@ -75,6 +121,13 @@ public class PlayerMove : MonoBehaviour
         }
         
       
+    }
+    
+    void GroundCheck()
+    {
+        IsGround = Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down).normalized * length, out RaycastHit hit, 1.2f);
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down).normalized * length, Color.magenta);
+
     }
 
     private bool WallCheck()
